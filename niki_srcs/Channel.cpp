b610ea40663f64810fd2ea7d6adcb77406a2d181
Niki42 <nik.7377@gmail.com>
2024-01-06 18:38:51 +0100

    add kick funktion and reneamed the maps

--- niki_srcs/Channel.cpp
+++ niki_srcs/Channel.cpp
@@ -2,13 +2,18 @@
 
 Channel::Channel(Client &owner, const std::string &channel_name) : _owner(owner), _name(channel_name)
 {
-	members.insert(std::pair<std::string, Client &>(owner.getUserName(), owner)); // is it nessesay to add him to the members??
-	operators.insert(std::pair<std::string, Client &>(owner.getUserName(), owner));
+	_members.insert(std::pair<std::string, Client &>(owner.getUserName(), owner)); // is it nessesay to add him to the _members??
+	_operators.insert(std::pair<std::string, Client &>(owner.getUserName(), owner));
 }
+Channel::Channel(const Channel &C) : _operators(C._operators), _members(C._members), _name(C._name), _owner(C._owner)
+{
+	
+}
+Channel::~Channel() {}
 									//maybe it cloud just be the user name
 void	Channel::sendMsgToChannel(Client &sender, const std::string &msg)
 {
-	for(members_itr member = members.begin(); member != members.end(); member++)
+	for(members_itr member = _members.begin(); member != _members.end(); member++)
 	{
 		if(member->second.getUserName() == sender.getUserName())
 			continue ;
@@ -20,19 +25,24 @@ void	Channel::sendMsgToChannel(Client &sender, const std::string &msg)
 	}
 }
 
-void	Channel::kickFromChannel(Client &kick_client)
+void	Channel::kickFromChannel(Client &executor, Client &kick_client)
 {
 	members_itr member;		// member that will be kicked
 
-	member = members.find(kick_client.getUserName());
-	if(member == members.end())
+	if(_operators.find(kick_client.getUserName()) == _operators.end())
+	{
+		std::cout << ">>? kickFromChannel() executor is not a operator" << std::endl;
+		return ;
+	}
+	member = _members.find(kick_client.getUserName());
+	if(member == _members.end())
 		std::cout << ">>? kickFromChannel() did not find the clinet to kick from channel: " << _name << std::endl;		// should we send a msg back that the client was not found to kick
 	else
-		members.erase(member);
+		_members.erase(member);
 }
 
 void	Channel::addClient(Client &new_member)
 {
 	// ### new_member.addChannel();				// add in client class the channel #############
-	members.insert(std::pair<std::string, Client &>(new_member.getNickName(), new_member));
+	_members.insert(std::pair<std::string, Client &>(new_member.getNickName(), new_member));
 }
--- niki_srcs/Channel.hpp
+++ niki_srcs/Channel.hpp
@@ -12,18 +12,22 @@ class Channel
 {
 	private:
 
-		std::map<std::string, Client &> operators; 
-		std::map<std::string, Client &> members;        // should it be just a std::string or a ref to the client?? should there be a operator map/vector
-		const std::string _name;                        // can we change it ?
+		std::map<std::string, Client &> _operators;
+		std::map<std::string, Client &> _members;		// should it be just a std::string or a ref to the client?? should there be a operator map/vector
+		std::string _name;                        		// can we change it ?
 		Client &_owner;									// should it be just a std::string or a ref to the client??
 
 		typedef std::map<std::string, Client &>::iterator members_itr;
+		
+		Channel();
 
 	public:
 
 		Channel(Client &owner, const std::string &channel_name);
+		Channel(const Channel &C);
+		~Channel();
 
 		void sendMsgToChannel(Client &sender, const std::string &msg);
-		void kickFromChannel(Client &kick_client);
+		void kickFromChannel(Client &executor, Client &kick_client);
 		void addClient(Client &new_member);
 };
