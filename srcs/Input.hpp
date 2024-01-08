1f3201d16c5843374bcf182e13006b91ca91aafc
SuQuoc <tran_quoc_su@hotmail.com>
2024-01-08 21:05:36 +0100

    pseudo code

---
+++ srcs/Input.hpp
@@ -0,0 +1,14 @@
+
+
+class input
+{
+    private:
+        std::string                 _prefix;
+        std::string                 _cmd;
+        std::vector<std::string>    _params;
+        std::string                 _msg;
+        unsigned int                param_size; 
+
+    
+
+}
