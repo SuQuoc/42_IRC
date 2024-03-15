2f07bf569eae6ca542c0da24d1c7cd7c611848b6
Niki42 <nik.7377@gmail.com>
2024-03-15 15:03:08 +0100

    hello

---
+++ tests/conection_tests/basicTests.py
@@ -0,0 +1,17 @@
+from tcp_client import tcpClient
+import asyncio
+
+async def main():
+    print("connect 2 users: ", end="")
+    async with tcpClient("niki") as niki, tcpClient("eule") as eule:
+        await niki.userJoin()
+        await eule.userJoin()
+        
+    print("ok")
+
+    print("stress test 1000 clients: ", end="")
+    await asyncio.gather(*(tcpClient(i).userJoinAndWrite() for i in range(1000)))
+    print("ok")
+
+
+asyncio.run(main())
--- tests/conection_tests/tcp_client.py
+++ tests/conection_tests/tcp_client.py
@@ -10,15 +10,15 @@ class tcpClient:
         self.name = name
 
     async def __aenter__(self):
-        self.reader, self.writer = await asyncio.open_connection(IP, PORT)
+        """ self.reader, self.writer = await asyncio.open_connection(IP, PORT) """
         return self
-    
+
     async def writeMsg(self, message):
         self.writer.write(message.encode())
         await self.writer.drain()
 
     async def readMsg(self):
-       data = await asyncio.wait_for(self.reader.read(600), 5)
+       data = await asyncio.wait_for(self.reader.read(600), 10)
        self.last_data = data.decode("utf-8")
 
     async def strcmpExit(self, data, cmpstr):
@@ -26,20 +26,31 @@ class tcpClient:
             print(f'\nReceived: {data!r}')
             print(f'\ncmpstr  : {cmpstr!r}')
             exit(1)
-        
+
+    async def userJoin(self):
+        self.reader, self.writer = await asyncio.open_connection(IP, PORT)
+        await self.writeMsg(f"PASS {PASS}\r\nUSER {self.name} 2 3 4\r\nNICK {self.name}\r\n")
+        await self.readMsg()
+        await self.strcmpExit(self.last_data, f":AfterLife 001 {self.name} :Welcome to the Internet Relay Network, {self.name}\r\n:AfterLife 005 {self.name} NICKLEN=9 CHANTYPES=#& CHANMODES=oiklt :are supported by this server\r\n")
+
+    async def userJoinAndWrite(self):
+        await self.userJoin()
+        await self.writeMsg(f"JOIN #666\r\n")
+        await self.readMsg()
+        l = self.last_data.split("\r\n")
+        await self.strcmpExit(l[0] + "\r\n", f":{self.name}!{self.name}@127.0.0.1 JOIN #666 * :{self.name}\r\n")
+        await asyncio.sleep(1)
+        for i in range(1000):
+            await self.writeMsg(f"PRIVMSG #666 :Under your nyancat is a bead\r\n")
+
     async def __aexit__(self, exc_type, exc, tb):
         self.writer.close()
         await self.writer.wait_closed()
         print(f"closed {self.name}")
 
-async def main():
-    async with tcpClient("niki") as user, tcpClient("eule") as eule:
-        await user.writeMsg(f"PASS {PASS}\r\nUSER {user.name} 2 3 4\r\nNICK {user.name}\r\n")
-        await user.readMsg()
-        await user.strcmpExit(user.last_data, f":AfterLife 001 {user.name} :Welcome to the Internet Relay Network, {user.name}\r\n:AfterLife 005 {user.name} NICKLEN=9 CHANTYPES=#& CHANMODES=oiklt :are supported by this server\r\n")
+""" async def main():
+    async with tcpClient("niki") as niki, tcpClient("eule") as eule:
+        await niki.userJoin()
+        await eule.userJoin()
 
-        await eule.writeMsg(f"PASS {PASS}\r\nUSER {eule.name} 2 3 4\r\nNICK {eule.name}\r\n")
-        await eule.readMsg()
-        await eule.strcmpExit(eule.last_data, f":AfterLife 001 {eule.name} :Welcome to the Internet Relay Network, {eule.name}\r\n:AfterLife 005 {eule.name} NICKLEN=9 CHANTYPES=#& CHANMODES=oiklt :are supported by this server\r\n")
-
-asyncio.run(main())
+asyncio.run(main()) """
